81b177fcc458af05783d8a0ac81b4834c1301271
John-Mark Gurney <jmg@funkthat.com>
2015-07-31 22:36:49 -0700

    quote so we get the semicolon...
    though there is the issue that setkey bails at the first error, so
    we can't exactly just run the full file..  looks like we need to
    directly run setkey from this script so we know..

--- genkeys.sh
+++ genkeys.sh
@@ -6,7 +6,7 @@ nbytes=$(echo -n "$longkey" | wc -c)
 
 for ealg in $(sort -u < esp.txt | awk '{print $1}'); do
 	for j in `jot $nbytes 1`; do
-		echo flush;
+		echo 'flush;'
 
 		keystr=$(echo $longkey | dd bs=1 count=$j 2>/dev/null)
 		echo 'add 192.168.6.1 192.168.6.2 esp 1383 -E '"$ealg"' "'"$keystr"'";'
@@ -15,7 +15,7 @@ done > keys.txt
 
 for aalg in $(sort -u < ah.txt | awk '{print $1}'); do
 	for j in `jot 256 1`; do
-		echo flush;
+		echo 'flush;'
 
 		keystr=$(echo $longkey | dd bs=1 count=$j 2>/dev/null)
 		echo 'add 192.168.6.1 192.168.6.2 ah 0x10001 -A '"$aalg"' "'"$keystr"'";'

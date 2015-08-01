dbe8ea1b64a8da19bd1c82db362ec2b6baee484e
John-Mark Gurney <jmg@funkthat.com>
2015-07-31 22:59:15 -0700

    limit the number of bytes to what the length of key is...
    directly execute the setkey command..

--- genkeys.sh
+++ genkeys.sh
@@ -5,19 +5,21 @@ longkey="12345678901234567890123456789012345678901234567890123456789012345678901
 nbytes=$(echo -n "$longkey" | wc -c)
 
 for ealg in $(sort -u < esp.txt | awk '{print $1}'); do
+	echo 'esp: ' $ealg
 	for j in `jot $nbytes 1`; do
+		(
 		echo 'flush;'
-
 		keystr=$(echo $longkey | dd bs=1 count=$j 2>/dev/null)
-		echo 'add 192.168.6.1 192.168.6.2 esp 1383 -E '"$ealg"' "'"$keystr"'";'
+		echo 'add 192.168.6.1 192.168.6.2 esp 1383 -E '"$ealg"' "'"$keystr"'";' ) | setkey -c
 	done
-done > keys.txt
+done
 
 for aalg in $(sort -u < ah.txt | awk '{print $1}'); do
-	for j in `jot 256 1`; do
-		echo 'flush;'
+	echo 'ah: ' $aalg
+	for j in `jot $nbytes 1`; do
+		( echo 'flush;'
 
 		keystr=$(echo $longkey | dd bs=1 count=$j 2>/dev/null)
-		echo 'add 192.168.6.1 192.168.6.2 ah 0x10001 -A '"$aalg"' "'"$keystr"'";'
+		echo 'add 192.168.6.1 192.168.6.2 ah 0x10001 -A '"$aalg"' "'"$keystr"'";' ) | setkey -c
 	done
-done >> keys.txt
+done

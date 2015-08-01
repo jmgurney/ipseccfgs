aa879b411293303d19d0c45cf5fd1362d7cf8358
John-Mark Gurney <jmg@funkthat.com>
2015-08-01 12:59:56 -0700

    add AH to the esp tests so that we can verify that the decryption really did happen properly.. the ping is probably enough, but lets be doublely sure...

--- mkesp.sh
+++ mkesp.sh
@@ -6,8 +6,8 @@ while read ealg nbits; do
 	keystr=$(echo $longkey | dd bs=1 count=$(($nbits / 8)) 2>/dev/null)
 
 	cat <<EOF > "esp.gen.$ealg.$nbits.conf"
-add 192.168.6.1 192.168.6.2 esp 1383 -E $ealg "$keystr";
-add 192.168.6.2 192.168.6.1 esp 1383 -E $ealg "$keystr";
+add 192.168.6.1 192.168.6.2 esp 1383 -E $ealg "$keystr" -A hmac-sha2-256 "12345678901234567890123456789012";
+add 192.168.6.2 192.168.6.1 esp 1383 -E $ealg "$keystr" -A hmac-sha2-256 "12345678901234567890123456789012";
 EOF
 
 done < esp.txt
